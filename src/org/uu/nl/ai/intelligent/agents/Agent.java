9cf6e4c7f266d0c5a3cccd9c2beb9eab8e509c28
Anca N <negulescuanca11@gmail.com>
2019-10-21 15:09:19 +0200

    saved file

--- src/org/uu/nl/ai/intelligent/agents/Agent.java
+++ src/org/uu/nl/ai/intelligent/agents/Agent.java
@@ -7,6 +7,10 @@ public class Agent {
 		
 		// 1. For each period query the available courses
 		// 2. Calculate utility taking into account preference and previous periods
+		// - have a class for schedule which contains a class for period which contains 2 courses which will have name lecturer blabla from the knowledge base
+		// and the calculated utility based on query???
+		// - populate the schedule period by period, find a clash, start a new branch where sth is changed that clash (prerequisites needed for highest utility course)
+		// - compare determined utilities
 		// 3. Write max function
 		
 		return util;
