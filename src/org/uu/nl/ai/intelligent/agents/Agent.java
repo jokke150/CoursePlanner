3314c483655ca683c7c127ef1b62642414fc69e8
jokke <jokke@jokke-ThinkPad-T440>
2019-10-27 23:48:26 +0100

    Update ontology and party implement agent

--- src/org/uu/nl/ai/intelligent/agents/Agent.java
+++ src/org/uu/nl/ai/intelligent/agents/Agent.java
@@ -2,9 +2,15 @@ package org.uu.nl.ai.intelligent.agents;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map.Entry;
 import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 import org.uu.nl.ai.intelligent.agents.data.CoursePlan;
@@ -29,12 +35,39 @@ public class Agent {
 
 	private List<CoursePlan> getBestCoursePlans(final CoursePlan coursePlan)
 			throws UnsupportedEncodingException, OWLOntologyCreationException, IOException {
-		final int util = 0;
 
-		// 1. For each period query the available courses
-		for (final String period : PERIODS) {
-			final Set<String> coursesInPeriod = QueryEngine.getInstance().getInstancesShortForm(period, false);
+		final List<CoursePlan> coursePlans = new ArrayList<>();
 
+		for (int period = coursePlan.getFirstIncompletePeriod(); period <= PERIODS.size(); period++) {
+			// 1. For each period query the available courses
+			final Set<String> coursesInPeriod = QueryEngine.getInstance().getInstancesShortForm(PERIODS.get(period),
+					false);
+			final SortedMap<Integer, Set<String>> coursesByUtility = getCoursesByUtility(coursesInPeriod);
+			for (final Entry<Integer, Set<String>> coursesByUtilityEntry : coursesByUtility.entrySet()) {
+				final int utility = coursesByUtilityEntry.getKey();
+				// TODO: Check for prerequisites differently and branch if it is feasible to
+				// take one in a previous period
+				final Set<String> validCoursesForUtility = coursesByUtilityEntry.getValue().stream()
+						.filter(i -> arePreconditionsFulfilled(i)).collect(Collectors.toSet());
+
+				for (final String course : validCoursesForUtility) {
+
+					if (validCoursesForUtility.size() > 1) {
+						// When a student has an option between two courses that are equally
+						// preferable, the student would like to take a course that her friend takes.
+
+						// Assumption: The more friends take a course the more preferable it is
+						// TODO: Correct? If yes, include in report!
+
+						final int numOfFriendsTakingCourse = getNumOfFriendsTakingCourse(course);
+
+						// TODO: branch if same utility and same numOfFriends
+						// bestCoursePlans.addAll(getBestCoursePlans(coursePlan));
+					}
+
+					coursePlan.addCourseInPeriod(course, period, utility);
+				}
+			}
 		}
 
 		// 2. Calculate utility taking into account preference and previous periods
@@ -47,11 +80,104 @@ public class Agent {
 		// - compare determined utilities
 		// 3. Write max function
 
-		return null;
+		return coursePlans;
+
 	}
 
-	private int calculateUtility(final String course) {
-		// TODO
-		return 0;
+	private SortedMap<Integer, Set<String>> getCoursesByUtility(final Set<String> courses)
+			throws UnsupportedEncodingException, OWLOntologyCreationException, IOException {
+		final SortedMap<Integer, Set<String>> coursesByUtility = new TreeMap<>();
+		for (final String course : courses) {
+			final int utility = calculateUtility(course);
+			Set<String> coursesForUtility;
+			if (coursesByUtility.containsKey(utility)) {
+				coursesForUtility = coursesByUtility.get(utility);
+			} else {
+				coursesForUtility = new HashSet<String>();
+			}
+			coursesForUtility.add(course);
+			coursesByUtility.put(utility, coursesForUtility);
+		}
+		return coursesByUtility;
 	}
+
+	private int calculateUtility(final String course)
+			throws UnsupportedEncodingException, OWLOntologyCreationException, IOException {
+		final QueryEngine queryEngine = QueryEngine.getInstance();
+
+		final Set<String> preferredCourses = this.preferences.getPreferredCourses();
+		final Set<String> friends = this.preferences.getFriends();
+		final Set<String> preferredTopics = this.preferences.getPreferredTopics();
+		final Set<String> preferredLecturers = this.preferences.getPreferredLecturers();
+		final Set<String> preferredDays = this.preferences.getPreferredDays();
+
+		int utility = 0;
+
+		if (preferredCourses.contains(course)) {
+			utility += this.preferences.getCoursesWeight();
+		}
+
+		final Set<String> courseTopics = queryEngine.getInstancesShortForm("taughtIn value " + course, false);
+		// Assumption: If a student prefers a parent topic he also prefers all subtopics
+		// TODO: Correct? If yes, include in report!
+		courseTopics.addAll(findParentTopics(courseTopics));
+
+		// TODO: Is the utility the same if the student prefers only one versus multiple
+		// of the topics taught in the course?
+		if (preferredTopics.stream().anyMatch(i -> courseTopics.contains(i))) {
+			utility += this.preferences.getTopicsWeight();
+		}
+
+		// TODO: Is the utility the same if the student prefers only one versus multiple
+		// of the lecturers teaching the course?
+		final Set<String> courseLecturers = queryEngine.getInstancesShortForm("teaches value " + course, false);
+		if (preferredLecturers.stream().anyMatch(i -> courseLecturers.contains(i))) {
+			utility += this.preferences.getLecturersWeight();
+		}
+
+		// TODO: Is the utility the same if the student prefers only one versus multiple
+		// of the days the course is taught on?
+		final Set<String> courseDays = queryEngine.getInstancesShortForm("comprisesCourse value " + course, false);
+		if (preferredDays.stream().anyMatch(i -> courseDays.contains(i))) {
+			utility += this.preferences.getDaysWeight();
+		}
+
+		return utility;
+	}
+
+	private Set<String> findParentTopics(final Set<String> topics)
+			throws UnsupportedEncodingException, OWLOntologyCreationException, IOException {
+		final Set<String> allParentTopics = new HashSet<String>();
+
+		for (final String topic : topics) {
+			final Set<String> topicParentTopics = QueryEngine.getInstance()
+					.getInstancesShortForm("isParentTopicOf value " + topic, false);
+			allParentTopics.addAll(topicParentTopics);
+			allParentTopics.addAll(findParentTopics(topicParentTopics));
+		}
+
+		return allParentTopics;
+	}
+
+	private int getNumOfFriendsTakingCourse(final String course)
+			throws UnsupportedEncodingException, OWLOntologyCreationException, IOException {
+		final Set<String> personsTakingCourse = QueryEngine.getInstance().getInstancesShortForm("takes value " + course,
+				false);
+		final Set<String> friends = this.preferences.getFriends();
+
+		final Set<String> friendsTakingCourse = new HashSet<String>(personsTakingCourse);
+		friendsTakingCourse.retainAll(friends);
+
+		return friendsTakingCourse.size();
+	}
+
+	private boolean arePreconditionsFulfilled(final String course) {
+		// A student cannot register for a course more than once. A student can take a
+		// course only if she has taken the prerequisite (if there is one) or a course
+		// similar to a prerequisite.
+
+		return false;
+
+	}
+
 }
--- src/org/uu/nl/ai/intelligent/agents/data/CoursePlan.java
+++ src/org/uu/nl/ai/intelligent/agents/data/CoursePlan.java
@@ -1,5 +1,6 @@
 package org.uu.nl.ai.intelligent.agents.data;
 
+import java.util.Objects;
 import java.util.Set;
 
 public class CoursePlan {
@@ -10,47 +11,133 @@ public class CoursePlan {
 	private Set<String> coursesInPeriod3;
 	private Set<String> coursesInPeriod4;
 
+	private int utility = 0;
+
 	public CoursePlan() {
 		super();
+	}
+
+	public Set<String> getCoursesInPeriod(final int period) {
+		switch (period) {
+		case 1:
+			return getCoursesInPeriod1();
+		case 2:
+			return getCoursesInPeriod2();
+		case 3:
+			return getCoursesInPeriod3();
+		case 4:
+			return getCoursesInPeriod4();
+		default:
+			throw new IllegalArgumentException();
+		}
+	}
+
+	public void addCourseInPeriod(final String course, final int period, final int utility) {
+		switch (period) {
+		case 1:
+			addCourseInPeriod2(course, utility);
+			break;
+		case 2:
+			addCourseInPeriod2(course, utility);
+			break;
+		case 3:
+			addCourseInPeriod2(course, utility);
+			break;
+		case 4:
+			addCourseInPeriod2(course, utility);
+			break;
+		default:
+			throw new IllegalArgumentException();
+		}
 	}
 
 	public Set<String> getCoursesInPeriod1() {
 		return this.coursesInPeriod1;
 	}
 
-	public void addCourseInPeriod1(final String courseInPeriod1) {
-		if (this.coursesInPeriod1.size() < NUM_OF_COURSES_PER_PERIOD) {
-			this.coursesInPeriod1.add(courseInPeriod1);
+	public void addCourseInPeriod1(final String courseInPeriod1, final int utility) {
+		Objects.requireNonNull(courseInPeriod1);
+
+		if (this.coursesInPeriod1.size() == NUM_OF_COURSES_PER_PERIOD) {
+			throw new IllegalStateException();
 		}
+		this.coursesInPeriod4.add(courseInPeriod1);
+		this.utility += utility;
 	}
 
 	public Set<String> getCoursesInPeriod2() {
 		return this.coursesInPeriod2;
 	}
 
-	public void addCourseInPeriod2(final String courseInPeriod2) {
-		if (this.coursesInPeriod2.size() < NUM_OF_COURSES_PER_PERIOD) {
-			this.coursesInPeriod2.add(courseInPeriod2);
+	public void addCourseInPeriod2(final String courseInPeriod2, final int utility) {
+		Objects.requireNonNull(courseInPeriod2);
+
+		if ((this.coursesInPeriod1.size() != NUM_OF_COURSES_PER_PERIOD)
+				|| (this.coursesInPeriod2.size() == NUM_OF_COURSES_PER_PERIOD)) {
+			throw new IllegalStateException();
 		}
+		this.coursesInPeriod4.add(courseInPeriod2);
+		this.utility += utility;
 	}
 
 	public Set<String> getCoursesInPeriod3() {
 		return this.coursesInPeriod3;
 	}
 
-	public void addCourseInPeriod3(final String courseInPeriod3) {
-		if (this.coursesInPeriod3.size() < NUM_OF_COURSES_PER_PERIOD) {
-			this.coursesInPeriod3.add(courseInPeriod3);
+	public void addCourseInPeriod3(final String courseInPeriod3, final int utility) {
+		Objects.requireNonNull(courseInPeriod3);
+
+		if ((this.coursesInPeriod1.size() != NUM_OF_COURSES_PER_PERIOD)
+				|| (this.coursesInPeriod2.size() != NUM_OF_COURSES_PER_PERIOD)
+				|| (this.coursesInPeriod3.size() == NUM_OF_COURSES_PER_PERIOD)) {
+			throw new IllegalStateException();
 		}
+		this.coursesInPeriod4.add(courseInPeriod3);
+		this.utility += utility;
 	}
 
 	public Set<String> getCoursesInPeriod4() {
 		return this.coursesInPeriod4;
 	}
 
-	public void addCourseInPeriod4(final String courseInPeriod4) {
-		if (this.coursesInPeriod4.size() < NUM_OF_COURSES_PER_PERIOD) {
-			this.coursesInPeriod4.add(courseInPeriod4);
+	public void addCourseInPeriod4(final String courseInPeriod4, final int utility) {
+		Objects.requireNonNull(courseInPeriod4);
+
+		if ((this.coursesInPeriod1.size() != NUM_OF_COURSES_PER_PERIOD)
+				|| (this.coursesInPeriod2.size() != NUM_OF_COURSES_PER_PERIOD)
+				|| (this.coursesInPeriod3.size() != NUM_OF_COURSES_PER_PERIOD)
+				|| (this.coursesInPeriod4.size() == NUM_OF_COURSES_PER_PERIOD)) {
+			throw new IllegalStateException();
+		}
+		this.coursesInPeriod4.add(courseInPeriod4);
+		this.utility += utility;
+	}
+
+	public int getUtility() {
+		return this.utility;
+	}
+
+	/**
+	 * @return 1 for Period 1, 2 for Period 2, 3 for Period 3, 4 for Period 4 and -1
+	 *         if the CoursePlan is finished
+	 */
+	public int getFirstIncompletePeriod() {
+		if (this.coursesInPeriod1.size() == NUM_OF_COURSES_PER_PERIOD) {
+			if (this.coursesInPeriod2.size() == NUM_OF_COURSES_PER_PERIOD) {
+				if (this.coursesInPeriod3.size() == NUM_OF_COURSES_PER_PERIOD) {
+					if (this.coursesInPeriod4.size() == NUM_OF_COURSES_PER_PERIOD) {
+						throw new IllegalStateException();
+					} else {
+						return 4;
+					}
+				} else {
+					return 3;
+				}
+			} else {
+				return 2;
+			}
+		} else {
+			return 1;
 		}
 	}
 
